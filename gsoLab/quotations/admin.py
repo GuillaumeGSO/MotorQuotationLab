026c62871c914e1aa41469e5b14d9a685e5533a1
Guillaume <guill@gui.com>
2021-05-21 00:53:06 +0200

    Step 2 : create models and some custom admin

--- gsoLab/quotations/admin.py
+++ gsoLab/quotations/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer, Quotation, Coverage
+
+# Models in the regular admin
+admin.site.register([Customer, Coverage])
+
+@admin.register(Quotation)
+class QuotationAdmin(admin.ModelAdmin):
+    list_display = ('quotationPrice', 'vehiculeModel')
+    ordering = ('created','modified')
+    search_fields = ['vehiculeModel']
--- gsoLab/quotations/models.py
+++ gsoLab/quotations/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.template.defaultfilters import date
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.TextField(max_length=50, null=False)
+    phone = models.TextField(max_length=12)
+    email = models.TextField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.name + ' (' + self.email +')'
+
+class Coverage(models.Model):
+    description = models.TextField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.description + ' - actual price : RM ' + str(self.price)
+
+class Quotation(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer_quotations')
+    vehiculeYearMake = models.PositiveSmallIntegerField()
+    vehiculeModel = models.TextField(max_length=80)
+    vehiculePrice = models.DecimalField(max_digits=10, decimal_places=2, default=100000, validators=[MinValueValidator(30000)])
+    quotationPrice = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    coverages = models.ManyToManyField(Coverage)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.short_date(self.created), self.customer.name, self.vehiculeModel)
+
+    def short_date(self, adate):
+        return date(adate, "Y/n/j")
+
+
