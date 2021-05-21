80ea6045621ddca5a588e6a49a8e3bbfa485fce2
Guillaume <guill@gui.com>
2021-05-21 20:39:46 +0200

    Change TextField to CharField

--- quotations/models.py
+++ quotations/models.py
@@ -4,15 +4,15 @@ from django.template.defaultfilters import date
 
 
 class Customer(models.Model):
-    name = models.TextField(max_length=50, null=False)
-    phone = models.TextField(max_length=12)
-    email = models.TextField(max_length=50, null=False)
+    name = models.CharField(max_length=50, null=False)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=50, null=False)
 
     def __str__(self):
         return self.name + ' (' + self.email +')'
 
 class Coverage(models.Model):
-    description = models.TextField(max_length=100)
+    description = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
@@ -21,7 +21,7 @@ class Coverage(models.Model):
 class Quotation(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer_quotations')
     vehiculeYearMake = models.PositiveSmallIntegerField()
-    vehiculeModel = models.TextField(max_length=80)
+    vehiculeModel = models.CharField(max_length=80)
     vehiculePrice = models.DecimalField(max_digits=10, decimal_places=2, default=100000, validators=[MinValueValidator(30000)])
     quotationPrice = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     coverages = models.ManyToManyField(Coverage)
