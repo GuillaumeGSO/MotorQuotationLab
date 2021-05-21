9d318e153fa7aed58934d7e449910a9323c791fc
Guillaume <guill@gui.com>
2021-05-21 10:19:16 +0200

    trying to make Step2 works

--- gsoLab/gsoLab/urls.py
+++ gsoLab/gsoLab/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from gsoLab.quotations.admin import quotAdmin 
 
 urlpatterns = [
     #regular admin
-    path('quotations-admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('quotations-admin/', quotAdmin.urls),
     path('', include('quotations.urls'))
 ]
--- gsoLab/quotations/admin.py
+++ gsoLab/quotations/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
+from django.contrib.admin.sites import AdminSite
 
-from .models import Customer, Quotation, Coverage
+from . import models
+
+class QuotationAdminSite(AdminSite):
+    site_header = "Quotations view by Agent"
+
+quotAdmin = QuotationAdminSite(name='quotAdmin')
+
 
 # Models in the regular admin
-admin.site.register([Customer, Coverage])
+admin.site.register([models.Customer, models.Coverage])
 
-@admin.register(Quotation)
-class QuotationAdmin(admin.ModelAdmin):
+
+#custom view for quotation admin
+class QuotationModelAdmin(admin.ModelAdmin):
     list_display = ('quotationPrice', 'vehiculeModel')
     ordering = ('created','modified')
     search_fields = ['vehiculeModel']
+    quotAdmin.register(models.Quotation)
