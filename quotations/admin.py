519f63d7bb18b86bdfceea04b1e2e6887be52db2
Guillaume <guill@gui.com>
2021-05-21 21:15:23 +0200

    Step 4 : send email admin action (mocked)

--- quotations/admin.py
+++ quotations/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.contrib.admin.sites import AdminSite
-from django.template.defaultfilters import register
+from django.contrib import messages
+from django.utils.translation import ngettext
 
 from . import models
+import traceback
+
 
 # Models in the regular admin
 admin.site.register([models.Customer, models.Coverage])
@@ -15,11 +18,28 @@ class QuotationAdminSite(AdminSite):
 quotAdmin = QuotationAdminSite(name='quotAdmin')
 
 
-#custom view for quotation admin
+
+
+def send_emails_service(qs):
+    for q in qs:
+        print(q.customer.email)
+    traceback.print_exc()
+    return len(qs)
+
 
 class QuotationModelAdmin(admin.ModelAdmin):
     list_display = ('customer','vehiculeModel','quotationPrice', 'modified')
     ordering = ('created','modified')
     search_fields = ['vehiculeModel']
+    actions = ['send_emails', ]
+    
+    @admin.action(description='Send email to user')
+    def send_emails(self, request, queryset):
+        sent = send_emails_service(queryset)
+        self.message_user(request, ngettext(
+            '%d email was successfully sent.',
+            '%d emails were successfully sent.',
+            sent,
+        ) % sent, messages.SUCCESS)
 
 quotAdmin.register(models.Quotation, QuotationModelAdmin)
