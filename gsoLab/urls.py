37985d194bb0abc72f6ae27697f55ea0d5e98330
Guillaume <guill@gui.com>
2021-05-21 10:38:17 +0200

    refactoring and make admin for agent works

--- gsoLab/urls.py
+++ gsoLab/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from gsoLab.quotations.admin import quotAdmin 
+from quotations.admin import quotAdmin 
 
 urlpatterns = [
     #regular admin
