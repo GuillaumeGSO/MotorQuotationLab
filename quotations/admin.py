c44f36d52a902b884a089c76e27b4d9763d33f56
Guillaume <guill@gui.com>
2021-05-21 20:41:06 +0200

    Custom Display on QuotationAdmin

--- quotations/admin.py
+++ quotations/admin.py
@@ -1,21 +1,25 @@
 from django.contrib import admin
 from django.contrib.admin.sites import AdminSite
+from django.template.defaultfilters import register
 
 from . import models
-
-class QuotationAdminSite(AdminSite):
-    site_header = "Quotations view by Agent"
-
-quotAdmin = QuotationAdminSite(name='quotAdmin')
-
 
 # Models in the regular admin
 admin.site.register([models.Customer, models.Coverage])
 
 
+class QuotationAdminSite(AdminSite):
+    site_header = "Quotations view by Agent"
+    site_title = "Quotation Admin"
+
+quotAdmin = QuotationAdminSite(name='quotAdmin')
+
+
 #custom view for quotation admin
+
 class QuotationModelAdmin(admin.ModelAdmin):
-    list_display = ('quotationPrice', 'vehiculeModel')
+    list_display = ('customer','vehiculeModel','quotationPrice', 'modified')
     ordering = ('created','modified')
     search_fields = ['vehiculeModel']
-    quotAdmin.register(models.Quotation)
+
+quotAdmin.register(models.Quotation, QuotationModelAdmin)
